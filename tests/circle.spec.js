008ea3b4fba0fe4d12b03a708e7549a1ff023ed2
Daniela Miyasato Goto <daniela.miyasato@live.com>
2022-01-17 19:04:04 -0300

    Requisito 4 - função circle - ok

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,14 +25,31 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(circle('1')).toBeUndefined();
+
     // Teste se circle retorna um objeto.
+    expect(typeof circle(2)).toBe('object');
+
     // Teste se o objeto retornado tem 3 propriedades.
+    // https://www.geeksforgeeks.org/find-the-length-of-a-javascript-object/
+    // https://pt.stackoverflow.com/questions/15058/como-saber-o-tamanho-quantidade-de-propriedades-atributos-de-um-objeto-em-ja
+    expect(Object.keys(circle(2)).length).toBe(3);
+
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle('')).toBeUndefined();
+
     // Teste se dentro do objeto retornado, a função retorna uma key com value igual a circunferência correta para um círculo de raio 2.
+    // https://jestjs.io/pt-BR/docs/using-matchers#n%C3%BAmeros - passado na aula ao vivo com Gian!
+    expect(circle(2).circumference).toBeCloseTo(12.56);
+
     // Teste se dentro do objeto retornado, a função retorna uma key com value igual a área correta para um círculo de raio 3.
+    expect(circle(3).area).toBeCloseTo(28.26);
+
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    // toEqual recursivamente verifica cada campo de um objeto ou array. - https://jestjs.io/pt-BR/docs/using-matchers
+    expect(circle(3)).toEqual({radius: 3, area: 28.259999999999998, circumference: 18.84});
+    
   });
 });

1707a9e6972dace33ad714086a106bcaca69ad14
Daniela Miyasato Goto <daniela.miyasato@live.com>
2022-01-18 11:24:06 -0300

    Requisito 5 - refatorando

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,12 +15,21 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => {
-  const estudante = {
+// const createStudent = (nome) => {
+//   const estudante = {
+//     name: nome,
+//     feedback: () => 'Eita pessoa boa!',
+//   };
+//   return estudante;
+// };
+
+// module.exports = createStudent;
+
+// vendo o vídeo do course (último exemplo) - https://app.betrybe.com/course/fundamentals/introducao-a-javascript-es6-e-testes-unitarios/javascript-es6-let-const-arrow-functions-e-template-literals/4411d706-403c-4d31-8b18-b491e9d22ea6/conteudos/065251d8-92dd-474d-bb5a-1a99eac853eb/parte-iii-arrow-functions/5538fcca-cb99-48ca-89e1-52fc94873784?use_case=side_bar 
+
+const createStudent = (nome) => ({
     name: nome,
     feedback: () => 'Eita pessoa boa!',
-  };
-  return estudante;
-};
-
-module.exports = createStudent;
+  });
+ 
+  module.exports = createStudent;
